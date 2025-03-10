dfe550db8085b8ad47ec5f3f89c7bc96934c739f
Doves <97999818+Doves34-34@users.noreply.github.com>
2025-03-10 23:34:44 +0000

    Update test eails.py

--- test eails.py
+++ test eails.py
@@ -3,7 +3,7 @@ import requests
 url = "https://privatix-temp-mail-v1.p.rapidapi.com/request/delete/id/%7Bmail_id%7D/"
 
 headers = {
-	"X-RapidAPI-Key": "a66cd04b83mshf9dc611913c244dp11dd70jsn8d2ebb3791d1",
+	"X-RapidAPI-Key": "",
 	"X-RapidAPI-Host": "privatix-temp-mail-v1.p.rapidapi.com"
 }
 
